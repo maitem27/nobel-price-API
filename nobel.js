d5f4cd3730f20793436c90df1492604a481fbe9e
maitem27 <mmertens4@student.efp.be>
2026-03-17 15:48:05 +0100

    correct mistake api

--- nobel.js
+++ nobel.js
@@ -1,13 +1,21 @@
 let btn = document.querySelector('#load');
 let gallery = document.querySelector('#gallery');
 
-i=0;
+
 
 btn.addEventListener('click', () => {
-    fetch('https://api.nobelprize.org/2.1/laureates')
+    fetch('https://api.nobelprize.org/2.1/nobelPrizes')
       .then(response => response.json())
       .then(nobel => {
-        console.log(nobel.laureates,nobel.laureates.length,nobel.laureates.awardYear)
+       console.log(nobel)
+
+//       let year = nobel[0].awardyear
+
+//       let nobel = 0;
+//       while (nobel < nobel.laureates.length) {
+    
+//       nobel = nobel + 1;
+// }
     });
 
 
