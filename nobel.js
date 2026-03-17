3a3203d0144bca5d6870bc2e0e9d849d0bc2dce5
maitem27 <mmertens4@student.efp.be>
2026-03-17 15:21:45 +0100

    fetch and then in the js

--- nobel.js
+++ nobel.js
@@ -1,7 +1,17 @@
-fetch('https://api.nobelprize.org/2.1/laureates')
-    .then(response => response.json())
-    .then(nobel =>{
-        console.log(nobel)
-    })
+let btn = document.querySelector('#load');
+let gallery = document.querySelector('#gallery');
 
-    
+i=0;
+
+btn.addEventListener('click', () => {
+    fetch('https://api.nobelprize.org/2.1/laureates')
+      .then(response => response.json())
+      .then(nobel => {
+        console.log(nobel.laureates,nobel.laureates.length,nobel.laureates.awardYear)
+    });
+
+
+
+
+
+})
